483cd5b35eb84629548cbcb26e5ac843d8f0fb6f
Lobachev Evgeniy <e.lobachev@bk.ru>
2022-09-02 14:37:02 +0400

    task_1

---
+++ task1/task_1.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("Max = " + num1);
+        }
+        else if (num1 < num2)
+        {
+            Console.WriteLine("Max = " + num2);
+        }
+    }
+}
