0936015221e61750c454a4fdad3d008773bc1329
Lobachev Evgeniy <e.lobachev@bk.ru>
2022-09-05 20:11:38 +0400

    task 2

--- task2/task_2.cs
+++ task2/task_2.cs
@@ -1,1 +1,14 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите третье число: ");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+        int max = Math.Max(num3, Math.Max(num1, num2));
+        Console.WriteLine($"Большее число : {max}");
+    }
+}
