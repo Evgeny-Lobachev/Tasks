96adf3f0a5de765bdfbf8b342eb5e7f76c17a9b1
Lobachev Evgeniy <e.lobachev@bk.ru>
2022-09-05 20:21:19 +0400

    task 3

---
+++ task3/task_3.cs
@@ -0,0 +1,17 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        
+        if (num % 2 == 0)
+        {
+            Console.Write("Четное число");
+        }
+        else
+        {
+            Console.Write("Нечетное число");
+        }
+    }
+}
